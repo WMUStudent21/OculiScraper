715ba2c629db56f354d5bad6ebf6f3196c8ea3cd
Michael Jia Hui Loh <mhg0694@wade.wmich.edu>
2022-06-15 14:47:46 -0400

    Working Version v1

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
+# 
 def extract(uri):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window() # For maximizing window
@@ -25,7 +26,7 @@ driver.maximize_window() # For maximizing window
 driver.implicitly_wait(60) # gives an implicit wait for 60 seconds
 driver.get(url)
 body = driver.find_element_by_xpath('/html/body')
-for i in range(600):
+for i in range(950):
     body.send_keys(Keys.PAGE_DOWN)
 
 with open('whitelist_oculus.txt') as f:
@@ -34,14 +35,17 @@ f.close()
 
 data = {}
 
-for title in game_search:    
-    link = driver.find_element(by=By.XPATH, value="//div[@class='store-section-item__meta-name' and contains(text(), '" + title + "')]/ancestor::div[@class='store-section-item']/a[@class='store-section-item-tile']").get_attribute("href")
-    desc = extract(link)
+for title in game_search:
+    try:   
+        link = driver.find_element(by=By.XPATH, value="//div[@class='store-section-item__meta-name' and contains(text(), '" + title + "')]/ancestor::div[@class='store-section-item']/a[@class='store-section-item-tile']").get_attribute("href")
+        desc = extract(link)
+    except:
+        desc = 'No description'
     curr = {    title : desc    }
     data.update(curr)
 
 f = open("output.txt", "w")
-f.write("{\n")
+f.write("{\n\n")
 for k in data.keys():
     f.write("'{}'\n===========================\n'{}'\n\n".format(k, data[k]))
 f.write("}")
