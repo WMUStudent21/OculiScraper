80347c048145e14989aa203d44dae167170aef8d
Michael Jia Hui Loh <mhg0694@wade.wmich.edu>
2022-06-15 14:20:21 -0400

    Fixed Oculus rendering

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 def extract(uri):
     driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -21,8 +22,11 @@ url = 'https://www.oculus.com/experiences/quest/section/1888816384764129'
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window() # For maximizing window
-driver.implicitly_wait(30) # gives an implicit wait for 20 seconds
+driver.implicitly_wait(60) # gives an implicit wait for 60 seconds
 driver.get(url)
+body = driver.find_element_by_xpath('/html/body')
+for i in range(600):
+    body.send_keys(Keys.PAGE_DOWN)
 
 with open('whitelist_oculus.txt') as f:
     game_search = f.read().splitlines() 
@@ -30,13 +34,11 @@ f.close()
 
 data = {}
 
-# with open('output.txt', 'w') as f:
 for title in game_search:    
     link = driver.find_element(by=By.XPATH, value="//div[@class='store-section-item__meta-name' and contains(text(), '" + title + "')]/ancestor::div[@class='store-section-item']/a[@class='store-section-item-tile']").get_attribute("href")
     desc = extract(link)
     curr = {    title : desc    }
     data.update(curr)
-    # print(curr, file=f)
 
 f = open("output.txt", "w")
 f.write("{\n")
